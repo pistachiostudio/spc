7ee9624cda0d220eea8a6c7084661deba872fe8b
quojama <giantkoyama@gmail.com>
2023-03-19 01:48:47 +0900

    type hint

--- run.py
+++ run.py
@@ -1,26 +1,26 @@
 import math
 
 class SkidPointCalculator:
-    def __init__(self, chain_ring: int, cog: int):
+    def __init__(self, chain_ring: int, cog: int) -> None:
         self.chain_ring = chain_ring
         self.cog = cog
 
-    def calculate_lcm(self):
+    def calculate_lcm(self) -> int:
         return math.lcm(self.chain_ring, self.cog)
 
-    def calculate_num_skid_points(self):
+    def calculate_num_skid_points(self) -> float | int:
         return round(self.calculate_lcm() / self.chain_ring, 2)
 
-    def calculate_gear_ratio(self):
+    def calculate_gear_ratio(self) -> float | int:
         return round(self.chain_ring / self.cog, 2)
 
-    def calculate_skid_point(self):
+    def calculate_skid_point(self) -> tuple[float | int, float | int]:
         return self.calculate_gear_ratio(), self.calculate_num_skid_points()
 
 # チェーンリングの歯の数の入力
 while True:
     try:
-        chain_ring = int(input('チェーンリングの歯の数を入力してください。: '))
+        chain_ring: int = int(input('チェーンリングの歯の数を入力してください。: '))
         break
     except ValueError:
         print('-----------------------')
@@ -30,7 +30,7 @@ while True:
 # コグの歯の数の入力
 while True:
     try:
-        cog = int(input('コグの歯の数を入力してください。: '))
+        cog: int = int(input('コグの歯の数を入力してください。: '))
         break
     except ValueError:
         print('-----------------------')
